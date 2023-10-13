0ce032d59e30e83670b8c72f134ecaadab37ed4b
AxelothLeohryn <alejandro97ss@gmail.com>
2023-10-13 12:26:33 +0200

    Ultimo ej sin terminar

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-document.querySelector('form').addEventListener('submit', function(event) {
+document.querySelector('#form1').addEventListener('submit', function(event) {
     event.preventDefault();
     let array = ['Name', 'Email', 'Mensaje', 'URL imagen']
-    let userData = [];
+    // let newArr = {`Name: ${event.target[0].value}, Email: ${event.target[1].value}, Mensaje: ${event.target[2].value}, URL imagen: ${event.target[3].value}`};
+    // console.log(newArr);
+    let userData = {};
     for (let i = 0; i < event.target.length; i++) {
-        localStorage.setItem(array[i], event.target[i].value)
-        userData.push({[array[i]]: event.target[i].value})
+        localStorage.setItem(array[i], event.target[i].value);
+        userData[array[i]] = event.target[i].value;
     }
-    console.log(userData);
     let tarjeta = document.querySelector('section');
     tarjeta.innerHTML += `
     <h2>Nombre: ${event.target[0].value}</h2>
@@ -14,6 +15,21 @@ document.querySelector('form').addEventListener('submit', function(event) {
             <p>Mensaje: ${event.target[2].value}</p>
             <img src="${event.target[3].value}" alt="">
     `
-
     localStorage.setItem("User Data", JSON.stringify(userData));
+    console.log(userData);
 })
+
+document.querySelector('#borrar-datos').addEventListener('click', function(event) {
+    localStorage.clear();
+});
+
+document.querySelector('#form3').addEventListener('submit', function(event) {
+    event.preventDefault();
+    console.log(event);
+    let userDelete = event.target[0].value;
+    let data = JSON.parse(localStorage.getItem('User Data'));
+    data = data.filter(item => item.Name !== event.target[0].value);
+    console.log(data);                 //sin terminar
+
+    
+});
